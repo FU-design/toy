fb31f21d20854c09058e5ad829b1076bc8e95ee9
fuliqiang <1348994179@qq.com>
2024-02-27 17:36:45 +0800

    优化根据文件目录及文件自动创建问题&解答页面

--- src/pages/question/modules/audition.ts
+++ src/pages/question/modules/audition.ts
@@ -5,10 +5,14 @@ import { BiBookHalf } from "@twist-space/vue3-icons/bi";
  * @note 解决无法使用 node 内置方法 require获取文件目录问题
  * https://cn.vitejs.dev/guide/migration-from-v2.html#importmetaglob
  */
-const modulesFiles = import.meta.glob("@/markdowns/audition/*.md");
+const modulesFiles = import.meta.glob("@/markdowns/audition/*.md", {
+  query: "?raw", // [?raw（将资源作为字符创引用） | ?url（将资源作为url引入）]
+  eager: true, // 必须要配置，才能生效
+  import: "default", // 默认
+});
 
 export const audition = {
   logo: BiBookHalf,
   category: "Audition",
-  qestionList: await getFileList(modulesFiles, "audition"),
+  qestionList: getFileList(modulesFiles, "audition"),
 };
