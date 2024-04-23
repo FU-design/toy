51d7c971adf557e72544db5e1f50f05432b6f0a8
fuliqiang <1348994179@qq.com>
2024-04-23 17:36:56 +0800

    组件CardBox样式修改

--- src/components/cardBox/CardBox.vue
+++ src/components/cardBox/CardBox.vue
@@ -20,18 +20,18 @@
 <style scoped>
 .card-box {
   padding: 1.6rem;
-  margin: 1.6rem;
+  margin: 1.6rem 0;
   border-radius: 0.8rem;
   /* box-shadow: rgba(10, 9, 9, 0.1) 0px 0px 5px; */
   box-shadow: 0 0.2rem 0.5rem 0.1rem rgba(64, 60, 67, 0.16);
   background-color: #ffffffa4;
-  overflow: auto;
+  /* overflow: auto; */
 }
 .card-box header {
   font-weight: 600;
 }
 .card-box main {
   width: 100%;
-  height: 100%;
+  height: auto;
 }
 </style>
