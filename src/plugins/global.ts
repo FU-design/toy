51337a245a1d04d7d130cc92abe1429a6fac65cf
fuliqiang <1348994179@qq.com>
2024-04-25 15:48:10 +0800

    对plugins的全局组件注册代码优化

--- src/plugins/global.ts
+++ src/plugins/global.ts
@@ -1,21 +1,27 @@
-import { App, defineAsyncComponent } from "vue";
+import { App, defineAsyncComponent as asyncComp } from "vue";
+
+const compMap = {
+  CardBox: "cardBox/CardBox",
+  CodeBlock: "codeBlock/CodeBlock",
+  Panel: "panel/Panel",
+  // Test: "",
+};
 
 export default {
   install(app: App, _options?: any) {
-    // 全局组件注册
-    app.component(
-      "CardBox",
-      defineAsyncComponent(() => import("@/components/cardBox/CardBox.vue"))
-    );
-
-    app.component(
-      "CodeBlock",
-      defineAsyncComponent(() => import("@/components/codeBlock/CodeBlock.vue"))
-    );
-
-    app.component(
-      "Panel",
-      defineAsyncComponent(() => import("@/components/panel/Panel.vue"))
-    );
+    // 对只有组件名，未提供组件路径或路径错误的抛出和捕获处理
+    try {
+      for (const [key, path] of Object.entries(compMap)) {
+        if (!path) {
+          throw new Error(`Component ${key} not found.`);
+        }
+        app.component(
+          key,
+          asyncComp(() => import(`@/components/${path}.vue`))
+        );
+      }
+    } catch (error) {
+      console.error("error :>> ", error);
+    }
   },
 };
