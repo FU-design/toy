0e6225a98a24a8224d5e9e2dcf869daf738e60f9
fuliqiang <1348994179@qq.com>
2024-04-25 16:39:57 +0800

    修改全局组件命名

--- src/plugins/comp.ts
+++ src/plugins/comp.ts
@@ -1,6 +1,6 @@
 import { App, defineAsyncComponent as asyncComp } from "vue";
 
-const compMap = {
+const baseCompMap = {
   CardBox: "cardBox/CardBox",
   CodeBlock: "codeBlock/CodeBlock",
   Panel: "panel/Panel",
@@ -10,7 +10,7 @@ const compMap = {
 export default (app: App) => {
   // 对只有组件名，未提供组件路径或路径错误的抛出和捕获处理
   try {
-    for (const [key, path] of Object.entries(compMap)) {
+    for (const [key, path] of Object.entries(baseCompMap)) {
       if (!path) {
         throw new Error(`Component ${key} not found.`);
       }
