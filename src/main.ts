901fc0b1afc0cd656a9967d5b930673aa54c0767
fuliqiang <1348994179@qq.com>
2024-05-06 09:33:41 +0800

    pinia 配置文件添加

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,12 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
-
-import "./theme/index.css";
+import pinia from "@/store/index";
 import plugin from "./plugins";
-// pinia 持久化插件引入
-import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
-
+import "./theme/index.css";
 const app = createApp(App);
-const pinia = createPinia();
 
 app.use(plugin);
 app.use(router);
-app.use(pinia.use(piniaPluginPersistedstate));
+app.use(pinia);
 app.mount("#app");
