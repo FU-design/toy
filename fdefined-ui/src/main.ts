00c22cccb9c8ab4045fa8e8647414d0ca9728b82
fuliqiang <1348994179@qq.com>
2024-07-26 10:37:52 +0800

    添加windowBeforeCloseConfirm方法

--- fdefined-ui/src/main.ts
+++ fdefined-ui/src/main.ts
@@ -3,25 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import pinia from "@/stores/index";
 import plugin from "./plugins";
+import { windowBeforeCloseConfirm } from "@/utils/tool";
 import "./theme/index.css";
-const app = createApp(App);
 
+windowBeforeCloseConfirm();
+const app = createApp(App);
 app.use(plugin);
 app.use(router);
 app.use(pinia);
-
 app.mount("#app");
-
-/**
- * 该监听事件，是对窗口关闭前的二次确认的操作
- */
-window.addEventListener("beforeunload", function (e) {
-  // 自定义提示信息（某些浏览器可能不显示这个信息）
-  const confirmationMessage = "你确定要离开此页面吗？未保存的数据将会丢失。";
-
-  // 现代浏览器设置提示信息
-  (e || window.event).returnValue = confirmationMessage;
-
-  // 对旧版浏览器设置返回值
-  return confirmationMessage;
-});
--- fdefined-ui/src/utils/tool.ts
+++ fdefined-ui/src/utils/tool.ts
@@ -53,3 +53,18 @@ export const copyText = (text: string): Promise<any> => {
     }
   });
 };
+
+// 对浏览器窗口关闭前的二次确认的操作
+export function windowBeforeCloseConfirm() {
+  window.addEventListener("beforeunload", function (e) {
+    e.preventDefault();
+    // // 自定义提示信息（某些浏览器可能不显示这个信息）
+    // const confirmationMessage = "你确定要离开此页面吗？未保存的数据将会丢失。";
+
+    // // 现代浏览器设置提示信息
+    // (e || window.event).returnValue = confirmationMessage;
+
+    // // 对旧版浏览器设置返回值
+    // return confirmationMessage;
+  });
+}
