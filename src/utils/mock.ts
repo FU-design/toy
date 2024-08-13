7742190f80ec7508e3e5d8527d07877e9cfdeb48
fuliqiang <1348994179@qq.com>
2024-08-13 16:36:53 +0800

    编写mock工具单元测试

--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -24,17 +24,20 @@ export const randomString = (length: number) => {
  * @returns {Promise<MockData>} 模拟接口数据
  */
 export const mockData = (size = 1000): Promise<MockData> => {
-  const data = new Array(size).fill(1).map((_v, i) => i + 1 + randomString(8));
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve({
-        code: 200,
-        data: data,
-        msg: "",
-      });
-      reject(404);
-    }, 500);
-  });
+  if (typeof size === 'number' && size >= 0) {
+    const data = new Array(size).fill(1).map((_v, i) => i + 1 + randomString(8));
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          code: 200,
+          data: data,
+          msg: "",
+        });
+      }, 500);
+    });
+  }
+  return Promise.reject(new Error("size must be greater than 0"));
+
 };
 
 /**
@@ -42,31 +45,26 @@ export const mockData = (size = 1000): Promise<MockData> => {
  * @returns
  */
 export const getImgs = (
-  size: number = 50,
-  width?: number,
-  height?: number
+  size: number = 30,
+  width: number = 400,
+  height: number = 300
 ): Promise<string[]> => {
-  return new Promise((resolve, reject) => {
-    try {
-      setTimeout(() => {
-        resolve(getImgsUrlList(size, width, height));
-      }, 200);
-    } catch (error) {
-      reject(error);
-    }
-  });
+  if (typeof size === 'number' && size >= 0) {
+    return new Promise((resolve, reject) => {
+      try {
+        setTimeout(() => {
+          resolve(getImgsUrlList(size, width, height));
+        }, 200);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+  return Promise.reject(new Error("size must be greater than 0"));
 };
 
 export const getImgsUrlList = (size: number, width?: number, height?: number) => {
   return Object.keys(new Array(size).fill(1)).map((_v, idx) => {
-    return `https://picsum.photos/id/${idx}/${width || 400}/${height || generateRandomNumber()
-      }`;
+    return `https://picsum.photos/id/${idx}/${width}/${height}`;
   });
-};
-
-const generateRandomNumber = () => {
-  // 生成随机宽度和高度，例如，这里设置最大宽高为 800 像素
-  const maxHeight = 200;
-  const randomHeight = Math.floor(Math.random() * maxHeight) + 1;
-  return Math.max(randomHeight, 50);
 };
---
+++ tests/unit/__utils__/mock.test.ts
@@ -0,0 +1,48 @@
+import { mockData, getImgs, getImgsUrlList } from '../../../src/utils/mock.ts'
+import { describe, expect, test, it, vi } from 'vitest'
+
+describe('mockData', () => {
+  it('should return data contain code, data, msg property', async () => {
+    const defaultSize = 1000
+    const respones = await mockData();
+    expect(respones).toHaveProperty('code', 200)
+    expect(respones).toHaveProperty('data')
+    expect(respones).toHaveProperty('msg')
+    expect(respones.data).toHaveLength(defaultSize)
+  })
+
+  it('should return 50 pieces of data after some time', async () => {
+    const size = 50
+    const respones = await mockData(50);
+    expect(respones.data).toHaveLength(size)
+  })
+
+  it('should reject with error if the request fails', async () => {
+    const size = -1
+    const promise = mockData(size)
+    await expect(promise).rejects.toThrow('size must be greater than 0')
+  })
+})
+
+
+describe('getImgs', async () => {
+  it('should handle default parameters correctly', async () => {
+    const defaultSize = 30
+    const imgList = await getImgs();
+    expect(imgList).toHaveLength(defaultSize)
+    expect(imgList.every(url => url.startsWith('https://picsum.photos/id/'))).toBe(true)
+  })
+
+  it('should return return 100 pictures of 500*500', async () => {
+    const size = 100
+    const imgList = await getImgs(size, 500, 500);
+    expect(imgList).toHaveLength(size)
+    expect(imgList.every(url => url.startsWith('https://picsum.photos/id/'))).toBe(true)
+  })
+
+  it('should reject with error if the request fails', async () => {
+    const size = -1
+    const promise = getImgs(size)
+    await expect(promise).rejects.toThrow('size must be greater than 0')
+  })
+})
