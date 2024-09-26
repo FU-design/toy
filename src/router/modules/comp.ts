7a201467ef831252ba403d07cb320f14932293d0
fuliqiang <1348994179@qq.com>
2024-09-26 16:39:08 +0800

    add tagTextMixInput router

--- src/router/modules/comp.ts
+++ src/router/modules/comp.ts
@@ -73,16 +73,16 @@ const routes = {
     //     menu: true,
     //   },
     // },
-    // {
-    //   path: "tagTextMixInput",
-    //   name: "TagTextMixInput",
-    //   component: () =>
-    //     import("@/pages/comp/component/tagTextMixInput/index.vue"),
-    //   meta: {
-    //     title: "TagTextMixInput",
-    //     menu: true,
-    //   },
-    // },
+    {
+      path: "tagTextMixInput",
+      name: "TagTextMixInput",
+      component: () =>
+        import("@/pages/comp/component/tag-text-mix-Input/index.vue"),
+      meta: {
+        title: "TagTextMixInput",
+        menu: true,
+      },
+    },
     {
       path: "xTable",
       name: "XTable",
