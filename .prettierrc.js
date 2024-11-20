385aa6b31aa2df472d54992e2116a2a49467f33d
fuliqiang <1348994179@qq.com>
2024-11-20 18:15:11 +0800

    chore: update eslint and prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,38 +1,20 @@
 export default {
-  printWidth: 100, // 一行最多 100 字符
-  useTabs: false, // 不使用缩进符，而使用空格
+  printWidth: 100, // 每行最多 100 字符
   tabWidth: 2, // 使用 2 个空格缩进
-  tabSize: 2,
-  semi: false, // 行尾需要有分号
-  singleQuote: true, // 使用单引号, 默认false(在jsx中配置无效, 默认都是双引号)
-  quoteProps: 'as-needed', // 对象的 key 仅在必要时用引号
-  jsxSingleQuote: false, // jsx 不使用单引号，而使用双引号
-  // 行尾逗号,默认none,可选 none|es5|all
-  // es5 包括es5中的数组、对象
-  // all 包括函数对象等所有可选
-  trailingComma: 'none',
-  // 对象中的空格 默认true
-  // true: { foo: bar }
-  // false: {foo: bar}
-  bracketSpacing: true,
-  // JSX标签闭合位置 默认false
-  // false: <div
-  //          className=""
-  //          style={{}}
-  //       >
-  // true: <div
-  //          className=""
-  //          style={{}} >
-  jsxBracketSameLine: false,
-  // 箭头函数参数括号 默认avoid 可选 avoid| always
-  // avoid 能省略括号的时候就省略 例如x => x
-  // always 总是有括号
-  arrowParens: 'always',
-  rangeStart: 0, // 每个文件格式化的范围是文件的全部内容
-  rangeEnd: Infinity,
-  requirePragma: false, // 不需要写文件开头的 @prettier
-  insertPragma: false, // 不需要自动在文件开头插入 @prettier
-  proseWrap: 'preserve', // 使用默认的折行标准
-  htmlWhitespaceSensitivity: 'css', // 根据显示样式决定 html 要不要折行
-  endOfLine: 'lf' // 换行符使用 lf 结尾是 \n \r \n\r auto
-};
+  useTabs: false, // 不使用制表符，而使用空格
+  semi: false, // 不使用分号
+  singleQuote: true, // 使用单引号
+  quoteProps: 'as-needed', // 仅在必要时为对象属性添加引号
+  jsxSingleQuote: false, // JSX 中使用双引号
+  trailingComma: 'none', // 禁用尾随逗号
+  bracketSpacing: true, // 在对象字面量的大括号之间保留空格
+  bracketSameLine: false, // 将 JSX 标签的闭合 > 放在下一行
+  arrowParens: 'always', // 箭头函数始终使用括号
+  rangeStart: 0, // 格式化文件的开始位置
+  rangeEnd: Infinity, // 格式化文件的结束位置
+  requirePragma: false, // 不需要文件头部的 @prettier 注释
+  insertPragma: false, // 不会自动在文件头部插入 @prettier
+  proseWrap: 'preserve', // 不自动折行
+  htmlWhitespaceSensitivity: 'css', // 遵循 CSS 显示样式的空格敏感性
+  endOfLine: 'lf' // 换行符使用 LF (\n)
+}
